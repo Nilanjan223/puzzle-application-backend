37038d748805476c6f3e374d6eb0a1e04f6a900a
Nilanjan Vyas <nilanjan.vyas2233@gmail.com>
2023-05-08 17:14:46 +0530

    final env

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://nvyas:yGutf41nCemDDzoy@cluster0.akj0uua.mongodb.net/elitmus?retryWrites=true&w=majority"
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.akj0uua.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(5000);
@@ -50,4 +50,4 @@ mongoose
     console.log(err);
   });
 
-// pass-yGutf41nCemDDzoy
+// pass-
